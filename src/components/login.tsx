5913b31e847b53879f1b61b02a7cf4774049fbb8
Iloniavo RANDRIAMANGA <99529538+Iloniavo@users.noreply.github.com>
2022-08-19 17:59:17 +0300

    Update login.tsx

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,94 +1,24 @@
-import React, {  useState } from 'react';
-import { signInWithRedirect , GoogleAuthProvider , signInWithPopup , signInWithEmailAndPassword , GithubAuthProvider , FacebookAuthProvider, createUserWithEmailAndPassword } from 'firebase/auth'
+import React, {  createContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import auth from '../config/config';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import FacebookIcon from '@mui/icons-material/FacebookRounded';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import GoogleIcon from '@mui/icons-material/Google';
 import { Box, Grid, makeStyles, Paper, Stack, Typography } from '@mui/material';
-
 import LoginIcon from '@mui/icons-material/Login';
 import Background from './Background';
 
 
+export default function Login(props:any){
 
-interface State {
-    password: string;
-    weight: string;
-    weightRange: string;
-    showPassword: boolean;
-  }
-
-export default function Login(){
-    const [values, setValues] = React.useState<State>({
-        password: '',
-        weight: '',
-        weightRange: '',
-        showPassword: false,
-      });
     const navigate = useNavigate();
     const [ loading , setLoading ] = useState<boolean>(false)
-    const [authing, setAuthing] = useState<boolean>(false)
     const [ email , setEmail ] = useState<string>("");
     const [ pass , setPassword ] = useState<string>("");
+    const [ res , setRes ] = useState<object>()
+   
     
- 
-    
-    const signInEmail = async () => {
-        signInWithEmailAndPassword(auth, email, pass)
-        .then((userCredential) => {
-    // Signed in 
-        const user = userCredential.user;
-        navigate('/')
-    // ...
-        })
-        .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode+ " Error code");
-    console.log(errorMessage+ " Error message");
-        });
-    }
-
-    const signInWithGoogle = async () => {
-       
-        setAuthing(true);
-        signInWithRedirect(auth, new GoogleAuthProvider()).then((response)=>{
-            console.log(response);
-            
-            navigate('/');
-        })
-        .catch((error)=>{
-            console.log(error);
-            setAuthing(false)
-        })
-    } 
-    const signInWithGithub = async () => {
-        setAuthing(true);
-        signInWithPopup(auth, new GithubAuthProvider()).then((response)=>{
-            console.log(response);
-            navigate('/');
-        })
-        .catch((error)=>{
-            console.log(error);
-            setAuthing(false)
-        })
-    }
-
-    const signInWithFacebook = async () => {
-        setAuthing(true);
-        signInWithPopup(auth, new FacebookAuthProvider()).then((response)=>{
-            console.log(response);
-            navigate('/');
-        })
-        .catch((error)=>{
-            
-            console.log(error);
-            setAuthing(false)
-        })
-    }
     function Copyright(props: any) {
         return (
           <Typography variant="body2" color="text.secondary" align="center" {...props}>
@@ -102,13 +32,12 @@ export default function Login(){
         );
       }
     
-     
-      
-    return (     
+    
+     return (     
      
        <Background>
-        <div className="contain" style={{width: '60%', margin: 'auto', opacity: '0.8', position: 'relative' , background: 'none' }}>
-            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} sx={{borderRadius:'5px' , background: 'none'}} square>
+        <div className="contain" style={{width: '60%', margin: 'auto', position: 'relative' , background: 'none' }}>
+            <Grid item xs={12} sm={8} md={5} component={Paper}  sx={{borderRadius:'5px' , background: 'none'}} square>
             <Box maxWidth={'xl'} sx={{
               my: 8,
               mx: 4,
@@ -116,14 +45,16 @@ export default function Login(){
               flexDirection: 'column',
               alignItems: 'center',
               }}>
-           
-            <h3 style={{paddingBottom :'2px'}}>Login  <LoginIcon/></h3>
+           <div style={{ display: 'flex', marginTop: '20px'}} > 
+           <h3 style={{paddingBottom :'2px'}}>Login</h3>
+           <LoginIcon sx={{paddingTop: '5px'}}  />
+           </div>
+            
             <div className='input'>
                 <Box component="form" noValidate sx={{ mt: 1 }} >
                    
                     <Grid item xs={12}>
                     <TextField 
-                    
                     margin="normal" 
                     fullWidth 
                     required 
@@ -134,7 +65,7 @@ export default function Login(){
                     variant="outlined" 
                     value={email}
                     onChange={(e:any) => setEmail(e.target.value)}
-                    sx={{ input : { color:'whitesmoke' }}}
+                    sx={{  color:'whitesmoke' }}
                      />
                     </Grid>
                     <Grid item xs={12}>
@@ -143,18 +74,17 @@ export default function Login(){
                         
                             <Stack direction="column" spacing={2}>
                                 
-                                <Button sx={{ fontSize: '2vh'  }} fullWidth onClick={()=> {navigate("/signup")}} variant="contained">Create an account</Button>
-                                <Button sx={{ fontSize: '2vh'}} fullWidth onClick={signInEmail} variant="contained">Sign in</Button>
+                                <Button sx={{ fontSize: '2vh'}} fullWidth onClick={props.signIn} variant="contained">Sign in</Button>
 
                                 <Stack direction="row" spacing={2} sx={{ justifyContent :'center', alignItems: 'center'}} >
                                         <Grid item xs={12} sm={4}>
-                                        <Button sx={{ fontSize: '1.8vh'}} fullWidth onClick={()=>signInWithGoogle()} variant="contained" color='error'><span> Google</span> <GoogleIcon/></Button>
+                                        <Button sx={{ fontSize: '1.8vh'}} fullWidth onClick={props.signWithGoogle} variant="contained" color='error'><span> Google</span> <GoogleIcon/></Button>
                                         </Grid>
                                         <Grid item xs={12} sm={4}>
-                                        <Button sx={{width :'10vw' , fontSize: '0.8rem'}} onClick={()=>signInWithGithub() } variant="contained" color='secondary'><span> github</span> <GitHubIcon/> </Button>
+                                        <Button sx={{width :'10vw' , fontSize: '0.8rem'}} onClick={props.signWithGithub} variant="contained" color='secondary'><span> github</span> <GitHubIcon/> </Button>
                                         </Grid>
                                         <Grid item xs={12} sm={4}>
-                                        <Button sx={{width :'10vw' , fontSize: '0.8rem'}} onClick={()=>signInWithFacebook()} variant="contained" color='primary'><span> facebook</span>  <FacebookIcon/></Button>
+                                        <Button sx={{width :'10vw' , fontSize: '0.8rem'}} onClick={props.signWithFacebook} variant="contained" color='primary'><span> facebook</span>  <FacebookIcon/></Button>
                                         </Grid>
                                     </Stack>
                             </Stack>  
