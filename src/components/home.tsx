83155561659ac72a7fe96d66cc5afec74bbdc483
Iloniavo RANDRIAMANGA <99529538+Iloniavo@users.noreply.github.com>
2022-08-19 17:59:55 +0300

    Update home.tsx

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
-import { signOut} from 'firebase/auth'
+import React, { useEffect, useState } from 'react';
+import { onAuthStateChanged, signOut, User} from 'firebase/auth'
 import auth from '../config/config';
 import { Navigate, useNavigate } from 'react-router-dom';
+import Background from './Background';
+import { getAuth } from "firebase/auth";
+import { Button } from '@mui/material';
 
-export default function Home(){
-    const navigate = useNavigate();
+export default function Home(props: any){
+        const auth = getAuth();
+        const [ name , setName ] = useState<string>("")
+        const [ picUrl , setPicUrl ] = useState<string>("")
+
+        useEffect(()=>{
+            const unsubscribe = onAuthStateChanged(auth, (user)=>{
+                console.log(user);
+                
+                if(user?.displayName && user.photoURL){
+                    setName(user.displayName)
+                    setPicUrl(user.photoURL)
+                } else if(user?.email){
+                    setName(user.email)
+                } else {
+                    navigate("/login")
+                }
+            }) 
+             return unsubscribe
+        }, [])
+  
+
+const navigate = useNavigate();
     const logout = ()=> {
     signOut(auth) 
     console.log('out');
@@ -12,9 +36,24 @@ export default function Home(){
    }
 
     return(
-            <div>
-                <p>Welcome to home page</p>
-                <button onClick={logout}>Sign out</button>
+        <Background>
+            <div className='home' >
+                <div className='picture'> 
+                <img src={picUrl} alt="picture.png" />
+                </div>
+                
+                <p style={{
+                    margin: '20px auto'
+                }}>Welcome {name}</p>
+                <div style={{
+                    width: '80%',
+                    margin: 'auto'
+                }}>
+                <Button onClick={logout} variant="contained">Sign out</Button>
+                </div>
+                
             </div>
+        </Background>
+            
     )
 }
